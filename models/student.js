c54774d99e9119c34e511bc0a33ae282fa954f6d
Joshua Equere <59533470+marveloper-8@users.noreply.github.com>
2020-10-07 14:35:58 +0100

    Update student.js

--- models/student.js
+++ models/student.js
@@ -41,6 +41,94 @@ const studentSchema = new mongoose.Schema({
      type:String,
      default:"https://res.cloudinary.com/cnq/image/upload/v1586197723/noimage_d4ipmd.png"
     },
+    maths: {
+        "default": "false",
+        "type": Boolean
+    },
+    english: {
+        "default": "false",
+        "type": Boolean
+    },
+    crk: {
+        "default": "false",
+        "type": Boolean
+    },
+    irk: {
+        "default": "false",
+        "type": Boolean
+    },
+    social_studies: {
+        "default": "false",
+        "type": Boolean
+    },
+    health_education: {
+        "default": "false",
+        "type": Boolean
+    },
+    computer: {
+        "default": "false",
+        "type": Boolean
+    },
+    spelling: {
+        "default": "false",
+        "type": Boolean
+    },
+    language: {
+        "default": "false",
+        "type": Boolean
+    },
+    diction: {
+        "default": "false",
+        "type": Boolean
+    },
+    basic_science: {
+        "default": "false",
+        "type": Boolean
+    },
+    business_studies: {
+        "default": "false",
+        "type": Boolean
+    },
+    civic: {
+        "default": "false",
+        "type": Boolean
+    },
+    basic_technology: {
+        "default": "false",
+        "type": Boolean
+    },
+    biology: {
+        "default": "false",
+        "type": Boolean
+    },
+    physics: {
+        "default": "false",
+        "type": Boolean
+    },
+    chemistry: {
+        "default": "false",
+        "type": Boolean
+    },
+    economics: {
+        "default": "false",
+        "type": Boolean
+    },
+    commerce: {
+        "default": "false",
+        "type": Boolean
+    },
+    accounting: {
+        "default": "false",
+        "type": Boolean
+    },
+    government: {
+        "default": "false",
+        "type": Boolean
+    },
+    agric_science: {
+        "default": "false",
+        "type": Boolean
+    }
 })
 
 mongoose.model("Student", studentSchema)
