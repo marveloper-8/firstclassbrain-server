66212b4991ebd2f67d45d35b2b7d21819461c46b
Joshua Equere <59533470+marveloper-8@users.noreply.github.com>
2020-10-07 11:14:53 +0100

    Update tests.js

--- routes/tests.js
+++ routes/tests.js
@@ -7,44 +7,31 @@ const requireLogin = require('../middleware/requireStudentLogin')
 const Test = mongoose.model("Test")
 // const Transaction = mongoose.model("Transaction")
 
-
-
 // tests
 router.post('/upload-test', (req, res) => {
-
-    const {
+    const { 
         type,
         classSelected,
         subject,
         term,
         week,
         topic,
-        ...questions
+        questions,
     } = req.body
-
-    const [data] = questions
-
-
-    const {
-        question,
-        answerA,
-        answerB,
-        answerC,
-        answerD,
-        correctAnswer,
-        correction,
-        correctionImage
-    } = data
-
-
-    // if(!type || !subject || !term || !week || !topic || !questions || !answerA || !answerB || !answerC || !answerD || !correctAnswer || !correction || !correctionImage){
-    //     return res.status(422).json({error: "Please add all the fields"})
-    // }
+    if(!type || !classSelected || !subject || !term || !week || !topic || !questions){
+        return res.status(422).json({error: "Please add all the fields"})
+    }
 
     // req.admin.password = undefined
     
     const test = new Test({
-        ...req.body
+        type,
+        classSelected,
+        subject,
+        term,
+        week,
+        topic,
+        questions
     })
     test.save().then(result => {
         return res.json({test: result})
@@ -98,33 +85,69 @@ router.get('/courses-detail/:id', (req, res) => {
     })
 })
 
-router.delete('/delete-post/:postId', requireAdminLogin, (req, res) => {
+
+
+router.delete('/delete-post/:postId', (req, res) => {
     Post.findOne({_id: req.params.postId})
     .populate("postedBy", "_id")
     .exec((err, post) => {
         if(err || !post){
             return res.status(422).json({error: err})
         }
-        if(post.postedBy._id.toString() === req.admin._id.toString()){
-            post.remove()
-            .then(result => {
-                res.json({message: "successfully deleted"})
-            }).catch(err => {
-                console.log(err)
-            })
-        }
+        post.remove()
+        .then(result => {
+            res.json({message: "successfully deleted"})
+        }).catch(err => {
+            console.log(err)
+        })
     })
 })
 
-router.post("/bought-course/:postId", (req,res) => {   
-    Post.findByIdAndUpdate(req.params._id, { paid:  true },
-    // Post.findByIdAndUpdate({ _id:  req.post.postId },   
+
+router.put('buy-course/:postId', (req, res) => {
+    var updatedRecord = {
+        paid: true
+    }
+
+    Post.findByIdAndUpdate(req.params._id, { $set: updatedRecord },{new:true}, (err, docs) => {
+        if (!err) res.send(docs)
+        else console.log('Error while updating a record : ' + JSON.stringify(err, undefined, 2))
+    })
+})
+
+router.post("/bought-course/:postId", (req,res) => {  
+    
+    Post.findByIdAndUpdate( req.params.postId, { $set: {paid: true} }, {useFindAndModify: false},
+    // Post.findByIdAndUpdate({ _id:  req.post.postId },
     function(err) {  
     if (err) {  
         res.send(err);  
         return;  
     }  
         res.send({data:"Record has been Updated..!!"});  
+    });  
+})  
+
+router.post("/buy--course/:postId", (req,res) => {  
+    let updatedRecord = {
+        courseTitle: "Edited"
+    }
+    
+    Post.findByIdAndUpdate( 
+        req.params._id, 
+        updatedRecord, 
+        {useFindAndModify: false},
+    // Post.findByIdAndUpdate({ _id:  req.post.postId },
+        function(err, updatedRecord) {  
+            if (err) {  
+                console.log("err", err)
+                res.send(err);  
+                return;  
+            }  else{
+                res.send({data:"Record has been Updated..!!"});  
+                res.send(updatedRecord)
+            }
+            
     });  
 })  
 
@@ -176,6 +199,58 @@ router.put('/comment',requireLogin,(req,res)=>{
     })
 })
 // end of posts
+
+
+
+
+
+
+
+
+
+
+
+router.get('/my-dashboard', requireLogin, (req, res) => {
+    Post.find({postedBy: req.student._id})
+        .populate("postedBy", "_id name")
+        .then(myPost => {
+            return res.json({myPost})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
+router.put('/like', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $push: {likes: req.student._id}
+    }, {
+        new: true
+    })
+    // .populate("likes", "-id firstName")
+    .exec((err, result) => {
+        if(err){
+            return res.status(422).json({error: err})
+        } else{
+            res.json(result)
+        }
+    })
+})
+
+router.put('/unlike', requireLogin, (req, res) => {
+    Post.findByIdAndUpdate(req.body.postId, {
+        $pull: {likes: req.student._id}
+    }, {
+        new: true
+    }).exec((err, result) => {
+        if(err){
+            return res.status(422).json({error: err})
+        } else{
+            res.json(result)
+        }
+    })
+})
+
 
 
 
