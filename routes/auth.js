50d223dd108199c188a448881c4a86b61a633303
unknown <dev@aoestatechariots.com>
2020-11-22 16:29:07 +0100

    admin reset password fix

--- routes/auth.js
+++ routes/auth.js
@@ -600,7 +600,7 @@ router.post('/admin/reset-password',(req,res)=>{
                     from:'"Firstclassbrain" <password@firstclassbrain.com>',
                     subject:"Password reset request",
                     text:   `You are receiving this because you (or someone else) have requested the reset of the password for your account.
-                    Please click on the following link, or paste this into your browser to complete the process: http://firstclassbrain.com/reset/admin/${token}
+                    Please click on the following link, or paste this into your browser to complete the process: http://admin.firstclassbrain.com/reset-password/${token}
                     If you did not request this, please ignore this email and your password will remain unchanged.`
                 }
 
